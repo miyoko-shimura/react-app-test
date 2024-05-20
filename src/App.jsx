c4526c8ef22680e30589effe39236353d2ef5f4d
Miyoko Shimura <120026227+miyoko-shimura@users.noreply.github.com>
2024-05-20 06:30:23 +0000

    change app.jsx
    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,70 @@
-import './App.css'
+import React, { useState } from "react"; 
 
-export default function App() {
-  return (
-    <main>
-      <h1>Happy Replit!</h1>
-    </main>
-  )
-}
+import "./App.css"; 
+
+function App() { 
+
+  const [display, setDisplay] = useState(""); 
+  return ( 
+
+    <main className="App"> 
+
+      <h1>Lovely Calculator</h1> 
+
+      <div className="container"> 
+
+        <form className="screen"> 
+
+          <input type="text" /> 
+
+        </form> 
+
+        <div className="keys"> 
+
+          <button id="clear">Clear</button> 
+
+          <button name="/" id="operation">&divide;</button> 
+
+          <button id="operation">del</button> 
+
+          <button name="7">7</button> 
+
+          <button name="8">8</button> 
+
+          <button name="9">9</button> 
+
+          <button name="*" id="operation">&times;</button> 
+
+          <button name="4">4</button> 
+
+          <button name="5">5</button> 
+
+          <button name="6">6</button> 
+
+          <button name="-" id="operation">-</button> 
+
+          <button name="1">1</button> 
+
+          <button name="2">2</button> 
+
+          <button name="3">3</button> 
+
+          <button name="+" id="operation">+</button> 
+
+          <button name="=" id = "result">=</button> 
+
+          <button name="0">0</button> 
+
+          <button name=".">.</button> 
+
+        </div> 
+
+      </div> 
+
+    </main> 
+
+  ); 
+
+} 
+
+export default App; 
