7d9d7d7d85e399bee2a7e28d9f0969e872f8e1b0
Miyoko Shimura <120026227+miyoko-shimura@users.noreply.github.com>
2024-05-20 09:19:38 +0000

    css jsx change
    css jsx change

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <main className="App">
-      <h1>React Simple Calculator by Miyoko.S</h1>
+      <h1>Simple Calculator by Miyoko.S</h1>
       <div className="container">
         <form className="screen">
           <input type="text" value={display} readOnly />
